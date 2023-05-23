87611e5edf7dbcd272a85e485fafa69ef93f126e
idawda <idawda@newrelic.com>
2023-05-23 18:48:20 +0530

    getting URL-Mappings for jax-rs(jersey)

---
+++ instrumentation-security/jersey/src/main/java/org/glassfish/jersey/server/internal/JerseyResourceContext_Instrumentation.java
@@ -0,0 +1,30 @@
+package org.glassfish.jersey.server.internal;
+
+import com.newrelic.api.agent.weaver.MatchType;
+import com.newrelic.api.agent.weaver.Weave;
+import com.newrelic.api.agent.weaver.Weaver;
+import com.nr.instrumentation.security.jersey.JerseyHelper;
+import org.glassfish.jersey.server.model.ResourceModel;
+
+
+@Weave(type = MatchType.ExactClass, originalName = "org.glassfish.jersey.server.internal.JerseyResourceContext")
+public class JerseyResourceContext_Instrumentation {
+
+    public void setResourceModel(ResourceModel resourceModel) {
+        System.out.println("inside initialize Jax-RS ");
+        try {
+            Weaver.callOriginal();
+        } finally {
+            postProcessing(resourceModel);
+        }
+    }
+
+
+    private static void postProcessing(ResourceModel resourceModel) {
+        try {
+            JerseyHelper.gatherUrlMappings(resourceModel);
+        } catch (Throwable ignored) {
+            ignored.printStackTrace();
+        }
+    }
+}
